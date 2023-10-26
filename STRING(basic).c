5517811b4d0286c58030b467bf5ea68c575408bb
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-10-26 22:24:18 +0530

    Create STRING(basic).c

---
+++ STRING(basic).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char s[100],i;    //100 is the size of char array 
+	printf("ENTER THE STRING:",i);
+	scanf("%s",s[i]);              //%s is used for string.....
+  //gets() can also be used in place of scanf..........
+	return 0;
+}
